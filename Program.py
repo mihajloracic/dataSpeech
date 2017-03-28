03283830bb4c565280a87dc8f12d8d592f9b53d1
mihajlo <mihajlo.rac@gmail.com>
2017-03-28 21:39:16 +0200

    inital commit

---
+++ Program.py
@@ -0,0 +1,66 @@
+class Row:
+    data = []
+    target = 0;
+    def __init__(self,data,target):
+        self.data = data;
+        self.target = target;
+    def __str__(self):
+        listStr = "";
+        for i in self.data:
+            listStr += str(i)+" ";
+        retVal = "";
+        retVal += "Data: " + listStr;
+        retVal += "\nTarget: " + str(self.target);
+        return retVal;
+import csv
+class Model:
+    rows = [];
+    def toListRow(self):
+        rowsList = [];
+        for row in self.rows:
+            rowsList.append(row.data);
+        return rowsList;
+    def toListTarget(self):
+        rowsList = [];
+        for row in self.rows:
+            rowsList.append(row.target);
+        return rowsList;
+    def addRow(self,valueList,target):
+        row = Row(valueList,target);
+        self.rows.append(row);
+count = -1
+trainingModel = Model();
+testModel = Model();
+model = Model();
+import random as r
+with open('train.csv', 'r') as csvfile:
+    spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
+    for row in spamreader:
+        count += 1;
+        
+        if(count == 0):
+            continue;
+        addThis = []
+        
+        for rec in row[0:1582]:
+            addThis.append(float(rec));
+        if(r.randint(1,10) == 3):
+            testModel.addRow(addThis,int(row[1582]));
+        else:
+            trainingModel.addRow(addThis,int(row[1582]));
+        model.addRow(addThis,int(row[1582]));
+
+        
+from sklearn.naive_bayes import GaussianNB
+
+gnb = GaussianNB()
+vracara = gnb.fit(trainingModel.toListRow(), trainingModel.toListTarget())
+y_pred = vracara.predict(testModel.toListRow())
+import numpy as np
+from sklearn.metrics import accuracy_score
+
+y_true = testModel.toListTarget();
+print(accuracy_score(y_true, y_pred))
+
+
+
