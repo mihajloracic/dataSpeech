1e1310ce59f6c93579e91d537c5c6dc4231617a6
mihajlo <mihajlo.rac@gmail.com>
2017-03-28 22:56:03 +0200

    izmene na test i traingn podacima

--- Program.py
+++ Program.py
@@ -6,15 +6,17 @@ class Row:
         self.target = target;
     def __str__(self):
         listStr = "";
-        for i in self.data:
-            listStr += str(i)+" ";
-        retVal = "";
-        retVal += "Data: " + listStr;
+        #for i in self.data:
+        #    listStr += str(i)+" ";
+        #retVal = "";
+        #retVal += "Data: " + listStr;
         retVal += "\nTarget: " + str(self.target);
         return retVal;
 import csv
 class Model:
     rows = [];
+    def __init__(self):
+        self.rows = [];
     def toListRow(self):
         rowsList = [];
         for row in self.rows:
@@ -31,36 +33,38 @@ class Model:
 count = -1
 trainingModel = Model();
 testModel = Model();
-model = Model();
 import random as r
 with open('train.csv', 'r') as csvfile:
     spamreader = csv.reader(csvfile, delimiter=',', quotechar='|')
     for row in spamreader:
-        count += 1;
-        
+        count += 1;        
         if(count == 0):
             continue;
         addThis = []
-        
+        testSelect = r.randint(1,200);
         for rec in row[0:1582]:
             addThis.append(float(rec));
-        if(r.randint(1,10) == 3):
+        if(testSelect == 3):
+            #print("Dodajem u test");
             testModel.addRow(addThis,int(row[1582]));
         else:
+            #print("Dodajem u trening set");
             trainingModel.addRow(addThis,int(row[1582]));
-        model.addRow(addThis,int(row[1582]));
-        
-        
+        if(count > 666):
+            break;
+print(len(trainingModel.toListTarget()))
+print(len(testModel.toListTarget()))
 from sklearn.naive_bayes import GaussianNB
 
-gnb = GaussianNB()
-vracara = gnb.fit(trainingModel.toListRow(), trainingModel.toListTarget())
+from sklearn import svm
+clf = svm.SVC()
+vracara = clf.fit(trainingModel.toListRow(), trainingModel.toListTarget())
 y_pred = vracara.predict(testModel.toListRow())
 import numpy as np
 from sklearn.metrics import accuracy_score
 
 y_true = testModel.toListTarget();
-#print(y_pred)
+#print(testModel.toListTarget())
 print(accuracy_score(y_true, y_pred))
 
 
